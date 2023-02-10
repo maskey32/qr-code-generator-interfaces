e0bbcbf2621950a766b0aa0061cf0bf0c7f16d28
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-10 23:51:46 +0100

    feat(Movie Component): Created movie component that displays list of movies from URL query string

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { MovieStyles } from './MovieStyles';
+import queryString from 'query-string';
 
 const Movie = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const search = queryString.parse(window.location.search);
+    const moviesArray = JSON.parse(search.movies);
+    
+    setMovies(moviesArray);
+  }, []);
+  
   return (
     <MovieStyles>
-      Movie component
+      <h1 className="movie-page-title">Generated Movies</h1>
+      <div className="movie-page-cards">
+        {movies.map((movie, index) => (
+          <div key={index} className="movie-card">
+            <h2 className="movie-card-title">{movie.title}</h2>
+            <img src={movie.image} alt={movie.title} className="movie-card-image" />
+          </div>
+        ))}
+      </div>
     </MovieStyles>
   )
 }
