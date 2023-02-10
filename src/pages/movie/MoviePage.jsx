5cc10fa86894dc731540f01578c237fa2f91948a
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-10 23:54:05 +0100

    feat(Components Import): Imported Movie and Footer components into movie page

--- src/pages/movie/MoviePage.jsx
+++ src/pages/movie/MoviePage.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Footer from '../../components/footer/Footer';
+import Movie from '../../components/movie/Movie';
 
 const MoviePage = () => {
   return (
     <div>
-      This is the movie page.
+      <Movie />
+      <Footer />
     </div>
   )
 }
