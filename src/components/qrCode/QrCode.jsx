810dd8abc2e0f97fc202bfcfff7d03fc71ffc9b8
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-10 16:56:20 +0100

    feat(QrCode Component): Created QrCode component to display qr code and regenerates a new QR code after 10 seconds

--- src/components/qrCode/QrCode.jsx
+++ src/components/qrCode/QrCode.jsx
@@ -1,10 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { QrCodeStyles } from './QrCodeStyles';
+import { HiOutlineCamera } from 'react-icons/hi';
+import { apiGet } from '../../utils/api';
 
 const QrCode = () => {
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    const fetchQRCodeUrl = async () => {
+      try {
+        const response = await apiGet('qrcode');
+        console.log('--response', response);
+        setUrl(response.data.result);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    const intervalId = setInterval(() => {
+      fetchQRCodeUrl();
+    }, 10000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const diagramUrl = 'https://www.kaspersky.com/content/en-global/images/repository/isc/2020/9910/a-guide-to-qr-codes-and-how-to-scan-qr-codes-1.jpg';
+
   return (
     <QrCodeStyles>
-      QrCode contents
+      <h1>QR Code Generator</h1>
+      <hr />
+      <main>
+        {/* <div className="left-side"> */}
+            <div className="diagram">
+                <img src={diagramUrl} alt="" />
+            </div>
+            <div className="text">
+                <HiOutlineCamera />
+                <p>Scan QR Code</p>
+            </div>
+        {/* </div> */}
+        <div className="right-side">
+          <img src={url} alt="QR code" />
+        </div>
+      </main>
     </QrCodeStyles>
   );
 };
