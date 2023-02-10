1a53a0d89f06354977ffbea743bdfc98d393285d
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-10 17:03:08 +0100

    feat(Footer Component): Created footer  component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 import { FooterStyles } from './FooterStyles';
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
+
   return (
     <FooterStyles>
-        Footer content
+        <p>{currentYear} &copy; Designed by Chukwuma Ezeobika. All rights reserved.</p>
+        <div className="container">
+            <h3>Contact</h3>
+            <p>+2348035033550</p>
+            <p>mezeobika32@gmail.com</p>
+        </div>
+        <p>Yoke Marketing Nigeria Coding Task.</p>
     </FooterStyles>
   )
 }
