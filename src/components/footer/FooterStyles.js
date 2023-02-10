f0f95f18d635a6a3ad95551456cb0fc1515263bf
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-10 17:04:43 +0100

    feat(Styles): Added footer component elements style

--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -1,5 +1,20 @@
 import styled from 'styled-components';
 
 export const FooterStyles = styled.div`
-    
+    width: 100%;
+    padding: 2% 5%;
+    background-color: #E0FFFF;
+    color: #000;
+    font-size: 1.1em;
+    display: flex;
+    align-items: top;
+    justify-content: space-between;
+
+    .container {
+        text-align: center;
+    }
+
+    .container h3 {
+        margin-bottom: 0.5em;
+    }
 `;
