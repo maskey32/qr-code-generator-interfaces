35190d9c0247744c51ae8ac4b56d66484c69060d
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-11 22:13:28 +0100

    chore(Restructure): Restructure the movie component elements

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import { BiPhone } from 'react-icons/bi';
+import { MdOutlineMail } from 'react-icons/md';
 import { FooterStyles } from './FooterStyles';
 
 const Footer = () => {
-    const currentYear = new Date().getFullYear();
+  const currentYear = new Date().getFullYear();
 
   return (
     <FooterStyles>
-        <p>{currentYear} &copy; Designed by Chukwuma Ezeobika. All rights reserved.</p>
-        <div className="container">
-            <h3>Contact</h3>
+      <p>
+        {currentYear} &copy; Designed by Chukwuma Ezeobika. All rights reserved.
+      </p>
+      <div className="container">
+        <h3>Contact</h3>
+        <div className="contact-info">
+          <div className="phone">
+            <BiPhone />
             <p>+2348035033550</p>
+          </div>
+          <div className="mail">
+            <MdOutlineMail />
             <p>mezeobika32@gmail.com</p>
+          </div>
         </div>
-        <p>Yoke Marketing Nigeria Coding Task.</p>
+      </div>
+      <p>Yoke Marketing Nigeria Coding Task.</p>
     </FooterStyles>
-  )
-}
+  );
+};
 
 export default Footer;
