b8d00e5eca734fcfd72390d3a93b6b4791adf3b7
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-11 22:15:04 +0100

    chore(Restyled): Restyled the movie component

--- src/components/footer/FooterStyles.js
+++ src/components/footer/FooterStyles.js
@@ -1,20 +1,60 @@
 import styled from 'styled-components';
 
 export const FooterStyles = styled.div`
-    width: 100%;
-    padding: 2% 5%;
-    background-color: #E0FFFF;
-    color: #000;
-    font-size: 1.1em;
+    width: 100vw;
     display: flex;
-    align-items: top;
     justify-content: space-between;
+    align-items: center;
+    padding: 2rem 5rem;
+    background-color: #333;
+    color: #fff;
+    font-family: Arial, sans-serif;
 
-    .container {
-        text-align: center;
+    p {
+        margin: 0;
+        padding: 0.5rem;
     }
 
-    .container h3 {
-        margin-bottom: 0.5em;
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+
+    h3 {
+        font-size: 1.5rem;
+    }
+
+    .contact-info {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        margin: 0.5rem 0;
+    }
+
+    .phone, .mail {
+        display: flex;
+        align-items: center;
+        gap: 0.5rem;
+    }
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        padding: 1rem;
+        align-items: center;
+        
+        h3 {
+            font-size: 1.2rem;
+        }
+
+        .container {
+            margin-top: 1rem;
+            margin-bottom: 1rem;
+        }
+
+        .contact-info {
+            flex-direction: column;
+        }
     }
 `;
