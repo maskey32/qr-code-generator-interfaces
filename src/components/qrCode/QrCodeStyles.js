fedea044068f1d432d63fcf84c3c2022fa38774c
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-11 22:19:09 +0100

    chore(Restyled): Restyled the qrcode component

--- src/components/qrCode/QrCodeStyles.js
+++ src/components/qrCode/QrCodeStyles.js
@@ -1,57 +1,118 @@
 import styled from 'styled-components';
 
 export const QrCodeStyles = styled.div`
-    width: 100%;
-    padding: 5%;
-    background-color: #4682B4;
-    color: #fff;
+  font-family: Arial, sans-serif;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  padding: 2rem;
+  background-color: #f9f9f9;
 
-    hr {
-        margin-top: 1em;
-    }
-    
-    main {
-        display: flex;
-        align-items: center;
-        justify-content: space-evenly;
-        margin: 4.3em 7em;
-        border: 2px solid #fff;
-        padding: 2em;
-    }
+  h1 {
+    font-size: 2rem;
+    margin-bottom: 1rem;
+    color: #333;
+  }
 
-    .left-side {
-        width: 40%;
-    }
-    
+  hr {
+    width: 80%;
+    border: 1px solid #ccc;
+    margin: 2rem 0;
+  }
+
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 80%;
+    margin: 2rem 0;
+    background-color: #f6d365;
+    padding: 1rem;
+    border-radius: 5px;
+    background-image: linear-gradient(to right, #00c6ff, #0072ff);
+
     .text {
-        display: flex;
-        align-items: center;
-        gap: 1em;
-        border: 2px solid #fff;
-        padding: 0.5em 1em;
-        font-size: 1.4em;
-        /* border-radius: 0.5em; */
-        user-select: none;
-    }
+      display: flex;
+      align-items: center;
+      font-size: 1.5rem;
+      color: #333;
+      margin-bottom: 1rem;
 
-    .diagram {
-        width: 40%;
+      p {
+        color: #fff;
+      }
+      
+      svg {
+        font-size: 2rem;
+        margin-right: 0.5rem;
+        color: #fff;
+      }
     }
-
-    .diagram img {
-        width: 100%;
-        /* margin-bottom: 2em; */
-        /* border-radius: 0.8em; */
-    }
-
-    .right-side {
-        background-color: #fff;
-        width: 28%;
-        height: 18em;
-        overflow: hidden;
-    }
-
-    .right-side img {
-        object-fit: cover;
-    }
+  }
 `;
+
+
+
+
+
+
+
+
+
+
+// import styled from 'styled-components';
+
+// export const QrCodeStyles = styled.div`
+//     width: 100%;
+//     padding: 5%;
+//     background-color: #4682B4;
+//     color: #fff;
+//     height: 80em;
+
+//     hr {
+//         margin-top: 1em;
+//     }
+    
+//     main {
+//         display: flex;
+//         flex-direction: column;
+//         align-items: center;
+//         margin: 4.3em 7em;
+//         border: 2px solid #fff;
+//         padding: 2em;
+//     }
+
+//     .left-side {
+//         width: 40%;
+//     }
+    
+//     .text {
+//         display: flex;
+//         align-items: center;
+//         gap: 1em;
+//         border: 2px solid #fff;
+//         padding: 0.5em 1em;
+//         font-size: 1.4em;
+//         user-select: none;
+//     }
+
+//     .diagram {
+//         width: 10%;
+//     }
+
+//     .diagram img {
+//         width: 100%;
+//     }
+
+//     .right-side {
+//         background-color: #fff;
+//         width: 75%;
+//         height: 50em;
+//         overflow: hidden;
+//     }
+
+//     .right-side img {
+//         object-fit: cover;
+//     }
+// `;
