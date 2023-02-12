466f6b2302ddf4cdeb1b98676ccf99681ad959e4
Chukwuma Michael Ezeobika <mezeobika32@gmail.com>
2023-02-12 18:09:14 +0100

    feat(User Experience): Added text display to users when QR code image is not displayed and when link is not navigating to the actual page

--- src/components/qrCode/QrCode.jsx
+++ src/components/qrCode/QrCode.jsx
@@ -36,8 +36,18 @@ const QrCode = () => {
           <HiOutlineCamera />
           <p>Scan QR Code</p>
         </div>
+        {!url && (
+          <div id='text'>
+            <p>Please be patient while the QR code loads. It may be due to a server or network glitch.</p>
+          </div>
+        )}
         <div className="right-side">
           <img src={url} alt="QR code" />
+        </div>
+        <div className="direction">
+          <p>
+            If you are encountering difficulties clicking on the scanned link or are being redirected to a blank page, please copy and paste the URL into your web browser.
+          </p>
         </div>
       </main>
     </QrCodeStyles>
